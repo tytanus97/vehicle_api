5ef624cb88b7f229035df3ca9349d5a786c05794
tytanus97 <tytanus97@gmail.com>
2020-10-19 20:27:07 +0200

    implement custom query for retrieving vehicle owners

--- src/main/java/org/example/repository/VehicleRepository.java
+++ src/main/java/org/example/repository/VehicleRepository.java
@@ -1,9 +1,18 @@
 package org.example.repository;
 
+import org.example.entity.Owner;
 import org.example.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-@NoRepositoryBean
+import java.util.Set;
+
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
+
+
+    @Query(value = "SELECT o FROM Owner o join fetch o.ownedVehicles ov where ov.vehicleId =:vehicleId")
+    Set<Owner> findVehicleOwners(@Param("vehicleId") Long vehicleId);
 }
