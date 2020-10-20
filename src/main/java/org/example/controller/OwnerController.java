0b8a6aa5009b3a9b1136b2fb504015bf04b99696
tytanus97 <tytanus97@gmail.com>
2020-10-20 11:52:41 +0200

    implement owner controller

--- src/main/java/org/example/controller/OwnerController.java
+++ src/main/java/org/example/controller/OwnerController.java
@@ -1,8 +1,36 @@
 package org.example.controller;
 
 
+import org.example.dto.OwnerDTO;
+import org.example.service.OwnerService;
+import org.example.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/owners")
 public class OwnerController {
+
+
+    private final OwnerService ownerService;
+    private final VehicleService vehicleService;
+
+
+    @Autowired
+    public OwnerController(OwnerService ownerService, VehicleService vehicleService) {
+        this.ownerService = ownerService;
+        this.vehicleService = vehicleService;
+    }
+
+
+    @GetMapping("/")
+    public ResponseEntity<List<OwnerDTO>> findAllOwners() {
+        retu
+    }
+
 }
