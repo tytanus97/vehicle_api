4a060ba933f9f1d798b6537c036253b6828a8e77
tytanus97 <tytanus97@gmail.com>
2020-10-20 21:08:34 +0200

    add bunch of unit tests for vehicle controller

--- src/test/java/org/example/controller/VehicleControllerTest.java
+++ src/test/java/org/example/controller/VehicleControllerTest.java
@@ -1,5 +1,8 @@
 package org.example.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.config.Config;
 import org.example.dto.CarDTO;
 import org.example.dto.RocketDTO;
 import org.example.dto.VehicleDTO;
@@ -10,10 +13,16 @@ import org.example.service.OwnerService;
 import org.example.service.VehicleService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.*;
 
@@ -21,8 +30,12 @@ import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 @WebMvcTest(VehicleController.class)
+@Import({Config.class})
 public class VehicleControllerTest {
 
     @MockBean
@@ -33,6 +46,12 @@ public class VehicleControllerTest {
 
     @Autowired
     VehicleController vehicleController;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @BeforeEach
     public void setUp() {
@@ -73,6 +92,40 @@ public class VehicleControllerTest {
         assertNotNull(result.getBody());
     }
 
+    @Test
+    public void getRequestForFindAllVehiclesShouldReturnResponseEntityWithStatusOK() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/vehicles/"))
+                .andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(),response.getStatus());
+
+    }
+
+    @Test
+    public void getRequestForFindAllVehiclesShouldReturnResponseEntityWithVehicleDTOList() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/vehicles/"))
+                .andReturn().getResponse();
+       List<VehicleDTO> resultList = Arrays.asList(objectMapper.readValue(response.getContentAsString(),VehicleDTO[].class));
+       assertFalse(resultList.isEmpty());
+    }
+
+    @Test
+    public void putRequestShouldReturnVehicleDTO() throws Exception {
+        VehicleDTO vehicleDTOBody = new CarDTO();
+        String bodyJSON = objectMapper.writeValueAsString(vehicleDTOBody);
+        MockHttpServletResponse response = mockMvc.perform(put("/vehicles/1")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(bodyJSON))
+                .andReturn().getResponse();
+
+
+        VehicleDTO updatedVehicleDTO = objectMapper.readValue(response.getContentAsString(),VehicleDTO.class);
+        System.out.println(updatedVehicleDTO.toString());
+        assertNotNull(updatedVehicleDTO);
+
+
+    }
+
     List<VehicleDTO> prepareData() {
         return Arrays.asList(new CarDTO(),new RocketDTO(1L,"Falcon 9","SpaceX",5000000,
                 2019,new HashSet<>(),2500,10,200),new CarDTO());
@@ -84,7 +137,6 @@ public class VehicleControllerTest {
 
     Set<Owner> prepareDataOwnerDTO() {
         return Set.of(new Owner(),new Owner(),new Owner());
-
     }
 
 }
