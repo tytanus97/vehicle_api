648bf6262104a8d941c2d926b17d114699d9d8b7
tytanus97 <tytanus97@gmail.com>
2020-10-21 23:36:47 +0200

    add swagger configuration

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,12 +2,28 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+@EnableSwagger2
 @SpringBootApplication
-public class App 
-{
+public class App{
+
     public static void main( String[] args )
     {
       SpringApplication.run(App.class,args);
     }
+
+    @Bean
+    public Docket swaggerConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+
 }
